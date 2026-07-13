c5f9e50ec7549759038a62733844ab662a068368
Anaïs BINET <anais.binet@fms-ea.com>
2026-07-13 13:35:16 +0200

    Ajout des docstrings dans game.py

--- game.py
+++ game.py
@@ -3,6 +3,19 @@ from player import Player
 
 
 def play_game():
+    """
+    Initialise une partie entre deux joueurs.
+
+    : parameter
+        Aucun.
+
+    : type
+        Aucun.
+
+    : return
+        player1 : premier joueur
+        player2 : second joueur
+    """
     player1 = Player(input("Joueur 1 nom ?"))
     player2 = Player(input("Joueur 2 nom ?"))
     player1.tour = True
@@ -11,6 +24,22 @@ def play_game():
 
 
 def input_game(joueur1, joueur2, node):
+    """
+    Récupère le coup du joueur dont c'est le tour.
+
+    : parameter
+        joueur1 : premier joueur
+        joueur2 : second joueur ou ordinateur
+        node : noeud actuel du jeu
+
+    : type
+        joueur1 : Player
+        joueur2 : Player
+        node : Node
+
+    : return
+        coup : nombre d'allumettes à retirer
+    """
     if joueur1.tour:
         print(f"C'est le tour de {joueur1.name} de jouer")
         coup = (input("Combien d'allumettes souhaite tu enlever ?"))
@@ -28,6 +57,22 @@ def input_game(joueur1, joueur2, node):
 
 
 def input_ordi(joueur, computer, node):
+    """
+    Détermine le coup joué par l'ordinateur.
+
+    : parameter
+        joueur : joueur humain
+        computer : joueur contrôlé par l'ordinateur
+        node : noeud actuel du jeu
+
+    : type
+        joueur : Player
+        computer : Player
+        node : Node
+
+    : return
+        coup : meilleur coup calculé par Minimax
+    """
     print(f"C'est le tour de {computer.name} de jouer")
     computer.tour = False
     joueur.tour = True
